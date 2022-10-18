927fe75aacd49f8333f54ac1f09dc35970025faf
Liam Welsh <liamwelsh17@gmail.com>
2022-10-18 17:13:11 -0300

    fix bugs

--- src/socket/sendFriendMessage.ts
+++ src/socket/sendFriendMessage.ts
@@ -4,13 +4,13 @@ import db from "../db/db";
 
 export default function sendFriendMessage(socket: Socket) {
   socket.on("sendFriendMessage", async (message: Message, room: string) => {
-    console.log("user", socket.handshake.auth.user);
-    socket.to(room).emit("recieveMessage", {
+    await db("messages").insert({
       userId: socket.handshake.auth.user.id,
       friendId: message.friendId,
       message: message.message,
     });
-    await db("messages").insert({
+
+    socket.to(room).emit("recieveMessage", {
       userId: socket.handshake.auth.user.id,
       friendId: message.friendId,
       message: message.message,
